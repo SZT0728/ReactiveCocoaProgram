4bcff533b27eb5550146b610ad43d0d5f8402fe9
SZT <1248300875@qq.com>
2016-03-18 19:37:46 +0800

    在第二个viewcontroller里面增加了RAC通知的另一种使用

--- ReativeCocoa/ReativeCocoa/SecondViewController.m
+++ ReativeCocoa/ReativeCocoa/SecondViewController.m
@@ -7,19 +7,55 @@
 //
 
 #import "SecondViewController.h"
+#import "ReactiveCocoa.h"
 
 @interface SecondViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *count;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property (weak, nonatomic) IBOutlet UIButton *loginbtn;
+
+
+@property(nonatomic,copy)NSString *countString;
 
 @end
 
 @implementation SecondViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
     UIBarButtonItem *leftbtn = [[UIBarButtonItem alloc]initWithTitle:@"back" style:(UIBarButtonItemStylePlain) target:self action:@selector(backAction:)];
     self.navigationItem.leftBarButtonItem = leftbtn;
+    
+    RAC(self.loginbtn,enabled) = [RACSignal combineLatest:@[self.count.rac_textSignal,self.password.rac_textSignal] reduce:^id{
+        return @(self.count.text.length > 6 && self.password.text.length >6);
+    }];
+    
+    [self.count.rac_textSignal subscribeNext:^(id x) {
+        
+        self.countString = (NSString *)x;
+
+    }];
+    
+    
+    //观察者的另一种写法
+//    [RACObserve(self, self.countString) subscribeNext:^(id x) {
+//        
+//        NSLog(@"self.countString变化了%@",self.countString);
+//    }];
+    
+    
+    //观察者还提供了过滤器
+    [[RACObserve(self, self.countString) filter:^BOOL(id value) {
+        return [value hasPrefix:@"h"];
+    }] subscribeNext:^(id x) {
+        NSLog(@"现在就是以h开头的值了 ：%@",x);
+    }];
+    
+
+
 }
 
 - (void)backAction:(UIBarButtonItem *)btn
